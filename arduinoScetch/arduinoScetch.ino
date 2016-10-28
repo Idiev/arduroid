ee3e3df075725c9a92389f950ef1b373c4d0ddb2
idiev <rasul.idiev@gmail.com>
2016-10-28 20:05:40 +0300

    First commit

---
+++ arduinoScetch/arduinoScetch.ino
@@ -0,0 +1,16 @@
+void setup()  
+ {  
+  Serial.begin(9600);  
+  pinMode(13, OUTPUT);
+ }  
+ void loop()  
+ {  
+  char c;
+  if(Serial.available())  
+  {  
+   c = Serial.read();  
+   Serial.print(c); 
+   if(c=='1') digitalWrite(13, HIGH);
+   else  digitalWrite(13, LOW);
+  }  
+ }  
